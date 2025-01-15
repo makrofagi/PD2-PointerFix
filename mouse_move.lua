11879046bc62a7ce45517cdcfd29efeb240887f1
makrofagi <fagi@ataxia.xyz>
2025-01-15 12:05:27 +0200

    fixed drawing in preplanning

--- mouse_move.lua
+++ mouse_move.lua
@@ -7,11 +7,10 @@ function MousePointerManager:_mouse_move(o, x, y)
     local delta_y = (y - current_y) * speed_modifier
 
     o:move(delta_x, delta_y)
-
+    
     self._ws:feed_mouse_position(o:world_position())
 
     if self._mouse_callbacks[#self._mouse_callbacks] and self._mouse_callbacks[#self._mouse_callbacks].mouse_move then
-        local new_x, new_y = o:world_position()
-        self._mouse_callbacks[#self._mouse_callbacks].mouse_move(o, new_x, new_y, self._ws)
+        self._mouse_callbacks[#self._mouse_callbacks].mouse_move(o, current_x, current_y, self._ws)
     end
 end
